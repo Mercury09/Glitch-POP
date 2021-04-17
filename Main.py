602b122e43c737c332d7a9147be95bfaa33e80f0
unknown <m.alibi143@gmail.com>
2021-04-17 14:14:37 +0600

    (fix)first commit

---
+++ Main.py
@@ -0,0 +1,66 @@
+ #Hello World
+
+# # print("Salem Alem")
+# print("Glitch-POP")
+# '''
+# sdasdas
+
+# '''
+# print("Ulpan Chert")
+
+#date types
+
+
+# data types
+
+# string
+# a = 'asdasd'
+# b = "asdasdasd"
+# c = '''asdasdasd'''
+
+# int
+# a = 1123
+
+# # float
+# a = 23.123
+
+# a1 = [1,2,3, '223']
+
+# d = {
+#   'asd': 123,
+#   '23': 233
+# }
+
+# # boolean
+# a = True
+# a = False
+
+
+# a = '123'
+# a = str(a)
+# print(a)
+# print(type(a))
+
+# a = 1
+# a = str(a)
+
+# print(bool(a))
+
+# a = range(5)
+# print(a)
+
+a = 1
+b = '1'
+c = 1.14
+
+a = float(a)
+b = float(b)
+c = float(c)
+d = a + b + c
+
+print(round(d,2))
+
+
+
+
+
